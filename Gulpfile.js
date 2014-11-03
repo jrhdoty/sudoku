2c81bf46ea6cc653eea3aacb9950c2918408214c
John Doty <jrhdoty@gmail.com>
2014-11-03 13:45:27 -0800

    add linting gulp task

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,5 +1,17 @@
-var gulp  = require('gulp');
-    $     = require('gulp-load-plugins');
-    karma = require('karma').server;
+var gulp        = require('gulp'),
+    $           = require('gulp-load-plugins')(),
+    karma       = require('karma').server,
+    browserify  = require('browserify'),
+    source      = require('vinyl-source-stream'),
+    streamify   = require('gulp-streamify'),
+    paths       = require('./paths.config.js');
 
-gulp.task
+
+
+gulp.task('lint', function(){
+  return gulp.src(paths.app_files.scripts)
+    .pipe($.jshint())
+    .pipe($.jshint.reporter('jshint-stylish'))
+    .pipe($.notify({message: 'Linting Done'}))
+    .pipe($.livereload());
+});
