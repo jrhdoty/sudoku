8287ad310e306c681267a9453f8e399dd8fea73b
John Doty <jrhdoty@gmail.com>
2014-11-06 09:52:14 -0800

    add class to table

--- app/scripts/src/gridView.js
+++ app/scripts/src/gridView.js
@@ -4,23 +4,24 @@ var View    = require('./view.js'),
 
 var GridView = View.extend({
 
-  el: '<table></table>',
+  el: '<table class="sudoku-grid"></table>',
 
   render: function(){
     var dim   = this.model.get('dim');
     var boxes = this.model.get('boxes');
 
     this.$el.addClass('grid');
-    return this.$el.append(_.map(boxes, function(row, idx){
-      return $('<tr>').append(_.map(row, function(box, idx){
+    this.$el.append(_.map(boxes, function(row, idx){
+      return $('<tr class="sudoku-row">').append(_.map(row, function(box, idx){
         return new BoxView({model: box}).render();
       }));
     }));
+
+    return this.$el;
   },
 
   initialize: function(){
-    this.model.on('changed', this.render, this);
-  }
+  },
 
 });
 
