cae904660a882137f7a52774374b4388d71e72a6
John Doty <jrhdoty@gmail.com>
2014-11-06 09:49:10 -0800

    update BoxView to properly add and remove classes on row/col conflict

--- app/scripts/src/boxView.js
+++ app/scripts/src/boxView.js
@@ -3,14 +3,34 @@ var View = require('./view.js'),
 
 
 var BoxView = View.extend({
-  el: '<div></div>',
+  el: '<td class="sudoku-box">\
+        <input class="sudoku-box-input" type="text"></input>\
+      </td>',
 
   render: function(){
-    return this.$el.html(this.model.get('value'));
+    var that = this;
+    this.$el
+    .find('input').val(this.model.get('value'))
+    .on('input', function(){
+      that.updateValue($(this).val());
+    });
+
+    if(this.model.get('conflict')){
+      this.$el.addClass('conflict');
+    } else {
+      this.$el.removeClass('conflict');
+    }
+
+    return this.$el;
   },
 
   initialize: function(){
     this.model.on('changed', this.render, this);
+  },
+
+  updateValue: function(val){
+    console.log('updating value of model to: ', val);
+    this.model.set('value', val);
   }
 
 });
