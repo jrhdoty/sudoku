ef12a7522d7f89476db3751d8d4bf09bb9b52eae
John Doty <jrhdoty@gmail.com>
2014-11-06 09:53:16 -0800

    add test to ensure "conflict" class is being properly added to cells

--- tests/boxView.spec.js
+++ tests/boxView.spec.js
@@ -2,22 +2,26 @@ var BoxView = require('../app/scripts/src/boxView.js');
 var Box     = require('../app/scripts/src/box.js');
 
 describe('boxView', function(){
-  var view, box, element;
+  var view, box, elem;
 
   beforeEach(function(){
     box = new Box({value: 10});
     view = new BoxView({model: box});
+    elem = view.render();
   });
 
   it('should correctly render with data from model', function(){
-    element = view.render();
-    expect(element.html()).to.equal('10');
+    expect(elem.find('input').val()).to.equal('10');
   });
 
   it('should correctly re-render when data from the model changes', function(){
-    element = view.render();
-    expect(element.html()).to.equal('10');
+    expect(elem.find('input').val()).to.equal('10');
     box.set('value', 0);
-    expect(element.html()).to.equal('0');
+    expect(elem.find('input').val()).to.equal('0');
+  });
+
+  it('should correctly add "conflict" class if conflict attribute is true', function(){
+    box.set('conflict', true);
+    expect(elem.attr('class').split(/\s+/).indexOf('conflict')).to.not.equal(-1);
   });
 });
